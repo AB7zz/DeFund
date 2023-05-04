0391d9a765944db0906b8303c8008879cfb0d817
Sameer-Abdulla <sameerabdwrk@gmail.com>
2023-05-04 17:46:35 +0530

    changed some Home.sx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const navigation = [
   { name: 'About', href: '#', current: false },
   { name: 'Join', href: '#', current: false },
   { name: 'Create', href: '#', current: false },
+  { name: 'My Chits', href: '#', current: false },
 ]
 
 function classNames(...classes) {
@@ -42,7 +43,7 @@ sm:items-stretch sm:justify-start">
                 <div className="hidden sm:ml-6 sm:block">
 
                   <div className="flex space-x-6">
-                  <h1 className="text-2xl font-bold pt-1
+                  <h1 className="text-2xl font-bold
 text-blue-800">DeFund</h1>
                     {navigation.map((item) => (
                       <a
@@ -71,12 +72,7 @@ items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white
 focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-
-src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <button className='bg-slate-800 text-white rounded px-5 py-2'>0xasdf234f2123</button>
                     </Menu.Button>
                   </div>
                   <Transition
