8a972ad68959ca90c31f96e600014f6ef1674e67
harshedabdulla <harshed2004@gmail.com>
2023-05-04 21:46:30 +0530

    rdfdm

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
 
-function About() {
+const About = () => {
   return (
-    <div>
-        <h3>About Us</h3>
+    <div className="flex flex-col items-center justify-center py-10 mt-20">
+      <h1 className="text-4xl font-bold mb-6">About Us</h1>
+      <div className="max-w-2xl text-center mb-6">
+        <p className="mb-4">
+        Chit funds are key instruments of financial inclusion for millions in India, especially for low
+budget investors who do not have access to formal institutions like banks and NBFCs. Chit
+fund is a very traditional and indigenous financial instrument helping subscribers to
+inculcate the habit of saving. India is home to millions of such subscribers who have been
+doing this for many decades now, but on the other hand chit fund system is also susceptible
+to fraud. With news reports claiming that between `1.2 to 1.4 lacs crores of public money is
+lost to various chit fund schemes. Upwards of 350 scams, impacting 15 crore families,
+across 17 states were identified which targeted primarily low budget investors by several
+chit fund companies. </p>
+        <p className="mb-4">
+          Blockchain technology offers numerous advantages over traditional chit funds, including increased transparency, security, and efficiency. By decentralizing the process, we're able to eliminate the need for intermediaries and reduce the risk of fraud or corruption.
+        </p>
+       
+        
+      </div>
+      <div className="max-w-2xl text-center border-2 p-4">
+        <h2 className="text-2xl font-bold mb-4">Tech Stack</h2>
+        <p className="mb-2">
+          Our platform is built using the following technologies:
+        </p>
+        <ul className="ml-6">
+          <li>React</li>
+          <li>Polygon</li>
+          <li>Solidity</li>
+          <li>ThirdWeb</li>
+          <li>Tailwind CSS</li>
+          
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
---
+++ client/src/components/CreateChitForm.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const CreateChitForm = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [installmentAmount, setInstallmentAmount] = useState(0);
+  const [period, setPeriod] = useState(0);
+  const [numParticipants, setNumParticipants] = useState(0);
+  const [deadline, setDeadline] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do something with form data, e.g. submit to backend API
+  };
+
+  return (
+    <div>
+        <Navbar />
+        <div className="flex justify-center">
+      <div className="w-1/4 text-center border-r-3 pr-5">
+        <h3 className="text-xl font-bold mb-4 mt-20">Rules for Chit Fund</h3>
+        <ul className="list-disc text-left">
+          <li>One person is selected every month to receive the funds</li>
+          <li>The amount is equally divided among all the participants</li>
+          <li>The person who receives the funds is determined by a random draw</li>
+          <li>Participants are required to pay a fixed amount every month</li>
+          <li>Participants cannot withdraw from the chit until the end of the term</li>
+        </ul>
+      </div>
+      <div className="w-2/3 pl-4 mt-20">
+        <h3 className="text-xl font-bold mb-4 text-blue-800">Create a New Chit</h3>
+        <form>
+          <label htmlFor="title" className="block font-bold mb-2">Title:</label>
+          <input type="text" id="title" name="title" className="w-full mb-4 p-2 border border-gray-400 rounded" />
+
+          <label htmlFor="description" className="block font-bold mb-2">Description:</label>
+          <textarea id="description" name="description" className="w-full mb-4 p-2 border border-gray-400 rounded" />
+
+          <label htmlFor="installmentAmount" className="block font-bold mb-2">Total Amount:</label>
+          <input type="number" id="totalAmount" name="totalAmount" className="w-full mb-4 p-2 border border-gray-400 rounded" />
+
+          <label htmlFor="installmentAmount" className="block font-bold mb-2">Installment Amount:</label>
+          <input type="number" id="installmentAmount" name="installmentAmount" className="w-full mb-4 p-2 border border-gray-400 rounded" />
+
+          <label htmlFor="period" className="block font-bold mb-2">Period (in days):</label>
+          <input type="number" id="period" name="period" className="w-full mb-4 p-2 border border-gray-400 rounded" />
+
+          <label htmlFor="participants" className="block font-bold mb-2">Number of Participants:</label>
+          <input type="number" id="participants" name="participants" className="w-full mb-4 p-2 border border-gray-400 rounded" />
+
+          <label htmlFor="deadline" className="block font-bold mb-2">Deadline:</label>
+          <input type="date" id="deadline" name="deadline" className="w-full mb-4 p-2 border border-gray-400 rounded" />
+
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create Chit</button>
+        </form>
+      </div>
+    </div>
+     <Footer />
+    </div>
+  );
+};
+
+export default CreateChitForm;
