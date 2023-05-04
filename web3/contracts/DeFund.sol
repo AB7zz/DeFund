ceedb7d235d7158228fa64d7e68552ca8ab428dc
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-05-04 15:27:33 +0530

    primary structure of smart contract completed

--- web3/contracts/DeFund.sol
+++ web3/contracts/DeFund.sol
@@ -2,23 +2,83 @@
 pragma solidity ^0.8.9;
 
 contract DeFund {
-    struct chitFunds {
-        uint256 id;
-        string name;
-        uint256 amount;
-        uint256 duration;
+    
+    struct ChitFund {
+        address creator;
+        string title;
+        string description;
         uint256 totalAmount;
-        uint256 monthlyAmount;
-        uint256 monthlyInterest;
-        uint256 monthlyTotalAmount;
-        uint256 monthlyPaidAmount;
-        uint256 monthlyPaidInterest;
-        uint256 monthlyPaidTotalAmount;
-        uint256 monthlyPaidDate;
-        uint256 monthlyPaidTime;
-        uint256 monthlyPaidStatus;
-        uint256 status;
-        uint256 createdDate;
-        uint256 createdTime;
+        uint256 amountCollected;
+        uint256 participantNum;
+        uint256 deadline;
+        address[] participants;
+        address[] paidParticipants;
+        uint256[] payments; 
+        address[] unpaidParticipants;
     }
+
+    mapping(uint256 => ChitFund) public ChitFunds;
+
+    uint256 public numberOfChitFunds = 0;
+
+    function createChitFunds(
+        address _creator, 
+        string memory _title, 
+        string memory _description, 
+        uint256 _totalAmount, 
+        uint256 _participantNum,   
+        uint256 _deadline
+        ) public returns (uint256) {
+
+        ChitFund storage chitFund = ChitFunds[numberOfChitFunds];
+
+        require(chitFund.deadline < block.timestamp, "Deadline must be in the future");
+
+        chitFund.creator = _creator;
+        chitFund.title = _title;
+        chitFund.description = _description;
+        chitFund.totalAmount = _totalAmount;
+        chitFund.participantNum = _participantNum;
+        chitFund.deadline = _deadline;
+        numberOfChitFunds++;
+
+        return numberOfChitFunds - 1;
+        
+    }
+
+    function joinChitFund(uint256 _id) public payable {
+
+        
+        ChitFund storage chitFund = ChitFunds[_id];
+        uint256 amount = chitFund.totalAmount / chitFund.participantNum;
+
+        chitFund.amountCollected += msg.value;
+        chitFund.participants.push(msg.sender);
+        chitFund.paidParticipants.push(msg.sender);
+
+        (bool sent, ) = payable(chitFund.creator).call{value: amount}("");
+        if (sent) {
+            chitFund.amountCollected = chitFund.amountCollected + amount;
+        }
+
+    }
+
+    function getParticipants(uint256 _id)
+        public
+        view
+        returns (address[] memory, uint256[] memory)
+    {
+        return (ChitFunds[_id].participants, ChitFunds[_id].payments);
+    }
+
+    function getChitFunds() public view returns (ChitFund[] memory) {
+        ChitFund[] memory allChitFunds = new ChitFund[](numberOfChitFunds);
+
+        for (uint256 i = 0; i < numberOfChitFunds; i++) {
+            ChitFund storage item = ChitFunds[i];
+            allChitFunds[i] = item;
+        }
+        return allChitFunds;
+    }
+
 }
